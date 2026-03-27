c85c66f871dda06558316a38c361b4b9a022f615
dgiron-2025071 <dgiron-2025071@kinal.edu.gt>
2026-03-27 14:51:09 -0600

    feat: Agregar el repository de DetalleVenta

---
+++ kinalapp/src/main/java/com/diegogiron/kinalapp/repository/DetalleVentaRepository.java
@@ -0,0 +1,7 @@
+package com.diegogiron.kinalapp.repository;
+
+import com.diegogiron.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+}
