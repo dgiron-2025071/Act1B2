411b527204c7e348fa1fd72d34ca21bc9ae06b2a
dgiron-2025071 <dgiron-2025071@kinal.edu.gt>
2026-03-27 21:42:23 -0600

    fix: cambiar estado y stock a Integer, agregar autoincrement

--- kinalapp/src/main/java/com/diegogiron/kinalapp/entity/Producto.java
+++ kinalapp/src/main/java/com/diegogiron/kinalapp/entity/Producto.java
@@ -8,35 +8,39 @@ import java.math.BigDecimal;
 public class Producto {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "codigo_producto")
     private int codigoProducto;
 
     @Column(name = "nombre_producto")
     private String nombreProducto;
+
     @Column(precision = 10, scale = 2)
     private BigDecimal precio;
+
     @Column
-    private int stock;
+    private Integer stock;
+
     @Column
-    private int estado;
+    private Integer estado;
+
     public Producto() {}
 
-    public Producto(int codigoProducto, String nombreProducto, BigDecimal precio, int stock, int estado) {
-        this.codigoProducto = codigoProducto;
+    public Producto(String nombreProducto, BigDecimal precio, Integer stock, Integer estado) {
         this.nombreProducto = nombreProducto;
         this.precio = precio;
         this.stock = stock;
         this.estado = estado;
     }
+
     public int getCodigoProducto() { return codigoProducto; }
     public void setCodigoProducto(int codigoProducto) { this.codigoProducto = codigoProducto; }
     public String getNombreProducto() { return nombreProducto; }
     public void setNombreProducto(String nombreProducto) { this.nombreProducto = nombreProducto; }
     public BigDecimal getPrecio() { return precio; }
     public void setPrecio(BigDecimal precio) { this.precio = precio; }
-    public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
-
-    public int getEstado() { return estado; }
-    public void setEstado(int estado) { this.estado = estado; }
+    public Integer getStock() { return stock; }
+    public void setStock(Integer stock) { this.stock = stock; }
+    public Integer getEstado() { return estado; }
+    public void setEstado(Integer estado) { this.estado = estado; }
 }
