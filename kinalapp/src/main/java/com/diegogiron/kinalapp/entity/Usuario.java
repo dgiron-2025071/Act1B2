464f438cabf443694d66ddd81c3f8c677f67e7a2
dgiron-2025071 <dgiron-2025071@kinal.edu.gt>
2026-03-27 21:48:30 -0600

    ix: cambiar estado de int a Integer en Usuario

--- kinalapp/src/main/java/com/diegogiron/kinalapp/entity/Usuario.java
+++ kinalapp/src/main/java/com/diegogiron/kinalapp/entity/Usuario.java
@@ -10,26 +10,32 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "codigo_usuario")
     private long codigoUsuario;
+
     @Column
     private String username;
+
     @Column
     private String password;
+
     @Column
     private String email;
+
     @Column
     private String rol;
+
     @Column
-    private int estado;
+    private Integer estado;
 
     public Usuario() {}
 
-    public Usuario(String username, String password, String email, String rol, int estado) {
+    public Usuario(String username, String password, String email, String rol, Integer estado) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.rol = rol;
         this.estado = estado;
     }
+
     public long getCodigoUsuario() { return codigoUsuario; }
     public void setCodigoUsuario(long codigoUsuario) { this.codigoUsuario = codigoUsuario; }
     public String getUsername() { return username; }
@@ -38,10 +44,8 @@ public class Usuario {
     public void setPassword(String password) { this.password = password; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public String getRol() { return rol; }
     public void setRol(String rol) { this.rol = rol; }
-
-    public int getEstado() { return estado; }
-    public void setEstado(int estado) { this.estado = estado; }
+    public Integer getEstado() { return estado; }
+    public void setEstado(Integer estado) { this.estado = estado; }
 }
